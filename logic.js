18ae9f4cbc6dd3e0162de7d14e506353a5ae1531
Sophia Dossland <sophia.dossland@gmail.com>
2017-03-03 14:16:47 -0800

    initial commit

---
+++ logic.js
@@ -0,0 +1,12 @@
+/**
+ * Created by sophia on 3/3/17.
+ */
+document.addEventListener("DOMContentLoaded", function () {
+
+  const script = document.createElement('script');
+  script.src('https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json');
+
+
+
+
+});
